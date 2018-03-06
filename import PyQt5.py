f11574227e7d69b507314a34929e4634d1e44779
mustafagokcen <gokcen_ergun@hotmail.com>
2018-03-06 21:21:21 +0300

    İlk Uygulama
    Title_olusturma

---
+++ import PyQt5.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.setWindowTitle('Simple')
+    w.show()
+    
+    sys.exit(app.exec_())
